a6ac5d223f2c7b9a3e5e739c87a3ba69e3b35ba2
VandevilleF <flo.vandeville@gmail.com>
2024-01-03 13:34:47 -0800

    Add condition if *stack == NULL

--- pint_pop.c
+++ pint_pop.c
@@ -32,7 +32,7 @@ void pop(stack_t **stack, unsigned int line_number)
  {
 	 stack_t *temp;
 
-	 if (stack == NULL)
+	 if (stack == NULL || *stack == NULL)
 	 {
 		 fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		 exit(EXIT_FAILURE);
