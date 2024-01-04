6481a16dd66bdcd50bdef0b423951a203de074b6
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-04 10:23:48 +0100

    deleted error message on push_pall which wasn't asked

--- push_pall.c
+++ push_pall.c
@@ -53,16 +53,11 @@ void pall(stack_t **stack, unsigned int line_number)
 	stack_t *new_node = *stack;
 
 	if (stack == NULL)
-	{
-		fprintf(stderr, "Error: Stack is NULL at line %u\n", line_number);
 		exit(EXIT_FAILURE);
-	}
-	else
+
+	while (new_node != NULL)
 	{
-		while (new_node != NULL)
-		{
-			printf("%d\n", new_node->n);
-			new_node = new_node->next;
-		}
+		printf("%d\n", new_node->n);
+		new_node = new_node->next;
 	}
 }
